eaf4282a47088eab721354af2f3742fccf469399
Aaditya Kothawade <aadityakothawade72@gmail.com>
2026-06-16 22:08:21 +0530

    database design and connectivity done

---
+++ app/(main)/dashboard/page.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function DashboardPage() {
+  return (
+    <div>
+        {/* Budget Progress */}
+
+        {/* overview  */}
+        
+        {/* Account Grid */}
+    
+    </div>
+  )
+}
+
+export default DashboardPage
---
+++ app/(main)/transaction/create/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      Transaction Page
+    </div>
+  )
+}
+
+export default page
