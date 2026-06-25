d61ae043539cb5da413d28d6fcaf900f382bc66b
Aaditya Kothawade <aadityakothawade72@gmail.com>
2026-06-25 21:30:40 +0530

    middleware size reduced

--- middleware.js
+++ middleware.js
@@ -27,26 +27,18 @@ const aj = arcjet({
 const clerk =  clerkMiddleware(async (auth,req)=>{
 
     const {userId} = await auth();
-
     if(!userId && isProtectedRoute(req)){
         const {redirectToSignIn} = await auth();
-
         return redirectToSignIn();
     }
-
-})
-
+});
 
 export default createMiddleware(aj,clerk);
 
-
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
     '/(api|trpc)(.*)',
-    // Always run for Clerk-specific frontend API routes
     '/__clerk/(.*)',
   ],
 }
